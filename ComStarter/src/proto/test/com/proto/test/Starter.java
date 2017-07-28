9d90a9a1ca88ecf823d7b4f551f67e379f6239d0
Sunil <sunhick@gmail.com>
2017-07-28 16:40:41 -0700

    working on protobuff

---
+++ ComStarter/src/proto/test/com/proto/test/Starter.java
@@ -0,0 +1,34 @@
+package com.proto.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		
+	}
+	
+//	public static List<MessageLite> getDefaultInstances() {
+////	    ArrayList<MessageLite> list = new ArrayList<MessageLite>();
+////	    // from src/main/proto/test.proto
+////	    list.add(ws.antonov.protobuf.test.Test.TestMessage.getDefaultInstance());
+////	    list.add(ws.antonov.protobuf.test.Test.AnotherMessage.getDefaultInstance());
+////	    list.add(ws.antonov.protobuf.test.Test.Item.getDefaultInstance());
+////	    list.add(ws.antonov.protobuf.test.Test.DataMap.getDefaultInstance());
+////	    // from src/main/proto/sample.proto (java_multiple_files == true, thus no outter class)
+////	    list.add(com.example.tutorial.Msg.getDefaultInstance());
+////	    list.add(com.example.tutorial.SecondMsg.getDefaultInstance());
+////	    // from lib/protos.tar.gz/stuff.proto
+////	    list.add(Stuff.Blah.getDefaultInstance());
+////	    // from ext/more.proto
+////	    list.add(More.MoreMsg.getDefaultInstance());
+////	    list.add(More.Foo.getDefaultInstance());
+////	    // from ext/test1.proto
+////	    list.add(Test1.Test1Msg.getDefaultInstance());
+////	    // from ext/test2.proto
+////	    list.add(Test2.Test2Msg.getDefaultInstance());
+//	    return list;
+//	  }
+
+}
