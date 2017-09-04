70cbb31d0fe231542ea24b03b9774ee71dc57bbb
Sunil <sunhick@gmail.com>
2017-09-04 10:15:14 -0700

    thread pool impl

--- patterns/src/main/java/com/threadpool/Starter.java
+++ patterns/src/main/java/com/threadpool/Starter.java
@@ -4,6 +4,9 @@ public class Starter {
 
 	public static void main(String[] args) {
 		System.out.println("Thread pool design pattern");
+		// This is already implemented in active object.
+		// Please take a look at scheduler and thread pool 
+		// implementation in active object.
 	}
 
 }
